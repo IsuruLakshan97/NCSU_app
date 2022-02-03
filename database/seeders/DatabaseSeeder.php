021b94e29b519d0b15794f79aff1f2b4ac11a4bb
Nadun Welikanda <nadunwelikanda.360@gmail.com>
2022-02-03 12:17:22 +0530

    changed seeding data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,14 +31,14 @@ class DatabaseSeeder extends Seeder
         }
 
         $department_list = [
-            ['id'=> 1, 'name'=> 'Department of Chemical & Process Engineering', 'faculty_id'=> 4],
-            ['id'=> 2, 'name'=> 'Department of Civil Engineering', 'faculty_id'=> 4],
-            ['id'=> 3, 'name'=> 'Department of Computer Engineering', 'faculty_id'=> 4],
-            ['id'=> 4, 'name'=> 'Department of Electrical & Electronic Engineering', 'faculty_id'=> 4],
-            ['id'=> 5, 'name'=> 'Department of Engineering Mathematics', 'faculty_id'=> 4],
-            ['id'=> 6, 'name'=> 'Department of Mechanical Engineering', 'faculty_id'=> 4],
-            ['id'=> 7, 'name'=> 'Department of Manufacturing & Industrial Engineering', 'faculty_id'=> 4],
-            ['id'=> 8, 'name'=> 'Department of Engineering Management', 'faculty_id'=> 4]
+            ['name'=> 'Department of Chemical & Process Engineering', 'faculty_id'=> 4],
+            ['name'=> 'Department of Civil Engineering', 'faculty_id'=> 4],
+            ['name'=> 'Department of Computer Engineering', 'faculty_id'=> 4],
+            ['name'=> 'Department of Electrical & Electronic Engineering', 'faculty_id'=> 4],
+            ['name'=> 'Department of Engineering Mathematics', 'faculty_id'=> 4],
+            ['name'=> 'Department of Mechanical Engineering', 'faculty_id'=> 4],
+            ['name'=> 'Department of Manufacturing & Industrial Engineering', 'faculty_id'=> 4],
+            ['name'=> 'Department of Engineering Management', 'faculty_id'=> 4]
         ];
 
         // If the table departments has no data, seed them with dummy data
