569486982003c1b37026893fcaba05a695bde35f
chandula <e16061@eng.pdn.ac.lk>
2022-02-04 15:53:46 +0000

    creat people table and person model

---
+++ database/migrations/2022_02_04_080817_create_people_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('people')) {
+            Schema::create('people', function (Blueprint $table) {
+                $table->id();
+                $table->string('fname', 20);
+                $table->string('lname', 20);
+                $table->string('fullname', 100);
+                $table->string('initial', 50);
+                $table->string('address', 100);
+                $table->string('city', 100);
+                $table->string('date', 20);
+                $table->string('regNo', 10);
+                $table->string('image', 200);
+                $table->integer('faculty_id');
+                $table->unsignedBigInteger('department_id');
+                $table->timestamps();
+
+                $table->foreign('department_id')->references('id')->on('departments');
+                $table->foreign('faculty_id')->references('id')->on('faculties');
+            });
+        }    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
