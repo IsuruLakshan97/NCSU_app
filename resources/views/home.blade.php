caa8af3a55929a1e6258f0a3a0cb6d14db714c2b
chandula <e16061@eng.pdn.ac.lk>
2022-02-06 16:05:43 +0000

    fix admin view and add a footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,7 +41,7 @@
             </tbody>
         </table>
     @else
-        <div class="row justify-content-center">
+        <!-- <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
@@ -57,7 +57,45 @@
                     </div>
                 </div>
             </div>
+        </div> -->git
+        <main class="container">
+            <h1>{{$user->faculty->name}}</h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Profile</li>
+                </ol>
+            </nav>
+        </main>
+        
+        <div class="row py-4">
+            <div class="col-md-2"></div>
+            <div class="d-grid gap-2 col-md-8">
+                @foreach($batch as $data)
+                <a role="button" class="btn btn-outline-primary" type="button" href="/person/{{$data->id}}">{{$data->batch}}</a>
+                @endforeach
+            </div>
         </div>
+        
     @endif
+</div>
+
+@endsection
+
+@section('footer')
+<div class="block">
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-muted">© 2022 University of Peradeniya</p>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+                <li class="nav-item"><a href="/forum/create" class="nav-link px-2 text-muted">Forum</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">People</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+            </ul>
+
+        </footer>
+    </div>
 </div>
 @endsection
