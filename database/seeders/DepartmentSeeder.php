2a4ed05fcfb276021a8341aa481829ccc4d28d6e
Nadun Welikanda <nadunwelikanda.360@gmail.com>
2022-02-09 12:33:44 +0530

    Department seeding has been added

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Faculty;
 use App\Models\Department;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DepartmentSeeder extends Seeder
 {
@@ -15,22 +15,35 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        $department_list = [
-            ['faculty_id'=> 4, 'name'=> 'Department of Chemical & Process Engineering'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Civil Engineering'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Computer Engineering'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Electrical & Electronic Engineering'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Engineering Mathematics'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Mechanical Engineering'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Manufacturing & Industrial Engineering'],
-            ['faculty_id'=> 4, 'name'=> 'Department of Engineering Management']
-        ];
+        // $department_list = [
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Chemical & Process Engineering'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Civil Engineering'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Computer Engineering'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Electrical & Electronic Engineering'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Engineering Mathematics'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Mechanical Engineering'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Manufacturing & Industrial Engineering'],
+        //     ['faculty_id'=> 4, 'name'=> 'Department of Engineering Management']
+        // ];
 
-        // If the table departments has no data, seed them with dummy data
-        if (DB::table('departments')->count() == 0) {
-            foreach ($department_list as $department) {
-                Department::create($department);
-            }
+        // // If the table departments has no data, seed them with dummy data
+        // if (DB::table('departments')->count() == 0) {
+        //     foreach ($department_list as $department) {
+        //         Department::create($department);
+        //     }
+        // }
+
+        
+        // How many department you need, defaulting to 10
+        $departmentCount = (int)$this->command->ask('How many department do you need for each faculty?', 10);
+        
+        $this->command->info("Creating {$departmentCount} departments for each...");
+        
+        $faculties = Faculty::select('id')->get();
+
+        // Create the Departments 
+        foreach ($faculties as $faculty) {
+            Department::factory()->count($departmentCount)->state(['faculty_id' => $faculty->id])->create();
         }
     }
 }
