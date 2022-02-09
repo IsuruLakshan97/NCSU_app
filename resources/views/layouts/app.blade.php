3e1032b8834ae900b028dfa70e2d349dbfaf1d93
chandula <e16061@eng.pdn.ac.lk>
2022-02-09 06:06:41 +0000

    add scrtipt section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'University of Peradeniya') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -98,4 +98,5 @@
     </div>
     @yield('footer')
 </body>
+@yield('script')
 </html>
