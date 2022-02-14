0cf6b2cb9e6cd5d3a705c3e6d89bf32d1ea141c4
chandula <e16061@eng.pdn.ac.lk>
2022-02-14 03:59:13 +0000

    remove cdn script

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -206,7 +206,6 @@
 @endsection
 
 @section('script')
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
 <script src="{{ asset('js/app.js') }}"></script>
 <script>
   $(document).ready(function() {
@@ -239,5 +238,5 @@
             {$('#department_id').empty();}
         });
     });
-  </script>
+</script>
 @endsection
