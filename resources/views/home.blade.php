71140b051673dd6c624f9112d8d7c476c84d3e34
chandula <e16061@eng.pdn.ac.lk>
2022-02-23 08:20:22 +0530

    change the title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,23 +56,6 @@
         </div>
         
     @else
-        <!-- <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div> -->
         <main class="container">
             <h1>Admin | {{$user->faculty->name}}</h1>
             <nav aria-label="breadcrumb">
